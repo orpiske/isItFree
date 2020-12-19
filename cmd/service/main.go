92c37887abf247eabd2a1365b9e95a3ecae6874f
Ricardo Lüders <ricardo@luders.com.br>
2020-12-19 20:37:31 +0100

    Adjusting main structure

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"time"
 
+	"github.com/orpiske/isItFree/pkg/recorder"
+
 	"github.com/orpiske/isItFree/pkg/parser"
 	"github.com/orpiske/isItFree/pkg/reader"
 )
@@ -12,34 +14,43 @@ import (
 func main() {
 	if len(os.Args) > 1 {
 		data, err := reader.FromLocal(os.Args[1])
-		if err != nil {
-			return
+		if err == nil {
+			parser.ParseGym(data)
 		}
-		parser.ParseGym(data)
 		return
 	}
 
 	gymURL := os.Getenv("IIF_GYM_SOURCE_URL")
 	poolURL := os.Getenv("IIF_POOL_SOURCE_URL")
 
-	for true {
+	rec := newRecorder()
+	for {
 
 		// @TODO Can be done inside goroutines
 		gdata, err := reader.FromWeb(gymURL)
-		if err != nil {
-			return
+		if err == nil {
+			gr, _ := parser.ParseGym(gdata)
+			rec.Record(gr)
 		}
-		parser.ParseGym(gdata)
 
 		// @TODO Can be done inside goroutines
 		pdata, err := reader.FromWeb(poolURL)
-		if err != nil {
-			return
+		if err == nil {
+			pr, _ := parser.ParsePool(pdata)
+			rec.Record(pr)
 		}
-		parser.ParsePool(pdata)
 
 		// @FIXME Using sleep is not a good idea
 		log.Print("Sleeping for 10 minutes")
 		time.Sleep(10 * time.Minute)
 	}
+}
+
+func newRecorder() recorder.Recorder {
+	// @TODO Make it better
+	recAdapter := os.Getenv("IIF_RECORDER_ADAPTER")
+	if recAdapter == "influx" {
+		return new(recorder.DiskRecorder)
+	}
+	return new(recorder.DiskRecorder)
 }
